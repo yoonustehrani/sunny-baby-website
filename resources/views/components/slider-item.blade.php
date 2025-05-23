deae4002f6f74588e53aafc7177351887356bc6c
yoonustehrani <yoonustehrani28@gmail.com>
2025-05-23 11:27:20 +0330

    slider fixed

--- resources/views/components/slider-item.blade.php
+++ resources/views/components/slider-item.blade.php
@@ -1,8 +1,8 @@
 <div class="swiper-slide" lazy="true">
     <div class="wrap-slider">
         <img class="lazyload" data-src="{{ $img }}" src="{{ $img }}" alt="fashion-slideshow-01">
-        <div class="box-content tw:flex tw:justify-end">
-            <div class="tw:flex tw:flex-col tw:px-12 tw:w-fit">
+        <div class="box-content tw:pl-6 tw:md:pl-12 tw:flex tw:justify-end">
+            <div class="tw:flex tw:flex-col tw:w-fit">
                 {{ $slot }}
             </div>
         </div>
