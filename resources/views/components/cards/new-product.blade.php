996b1492f5442422f475622fd06bb3399c380df2
yoonustehrani <yoonustehrani28@gmail.com>
2025-06-07 20:23:15 +0330

    add to cart button new

--- resources/views/components/cards/new-product.blade.php
+++ resources/views/components/cards/new-product.blade.php
@@ -29,8 +29,9 @@
         <a href="home-04.html#quick_add" data-bs-toggle="modal" class="btn-quick-add quick-add tw:hidden tw:md:flex">@lang('QUICK ADD')</a>
     </div>
     <div class="card-product-info tw:px-2!">
-        <a href="#" class="title link tw:text-xl">{{ $title }}</a>
-        <span class="price tw:text-lg">{{ format_price($price) }}</span>
+        <a href="#" class="title link tw:text-xl tw:mx-auto tw:font-semibold">{{ $title }}</a>
+        <span class="price tw:text-lg tw:mx-auto tw:font-normal">{{ format_price($price) }}</span>
+        <a class="tw:block tw:rounded-md tw:shadow-sm tw:border tw:border-transparent tw:duration-300 tw:hover:border-gray-700 tw:bg-sunset tw:px-3 tw:py-1 tw:my-4 tw:mx-auto" href="#">@lang('QUICK ADD')</a>
         {{-- <ul class="list-color-product">
             <li class="list-color-item color-swatch active">
                 <span class="tooltip">Grey</span>
