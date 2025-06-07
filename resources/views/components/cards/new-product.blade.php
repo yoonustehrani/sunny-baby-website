31d75f2e93aed002678a1472368ae5281f26d031
yoonustehrani <yoonustehrani28@gmail.com>
2025-06-07 23:24:46 +0330

    minor style change

--- resources/views/components/cards/new-product.blade.php
+++ resources/views/components/cards/new-product.blade.php
@@ -1,4 +1,4 @@
-<div class="card-product tw:bg-alabaster! tw:p-2! tw:rounded-xl style-4 fl-item">
+<div class="card-product tw:bg-white! tw:p-2! tw:rounded-xl style-4 fl-item">
     <div class="card-product-wrapper">
         <a href="product-detail.html" class="product-img">
             <img class="lazyload img-product" data-src="{{ asset('images/products/photo-6.jpg') }}" src="{{ asset('images/products/photo-6.jpg') }}" alt="image-product">
@@ -29,8 +29,8 @@
         <a href="home-04.html#quick_add" data-bs-toggle="modal" class="btn-quick-add quick-add tw:hidden tw:md:flex">@lang('QUICK ADD')</a>
     </div>
     <div class="card-product-info tw:px-2!">
-        <a href="#" class="title link tw:text-xl tw:mx-auto tw:font-semibold">{{ $title }}</a>
-        <span class="price tw:text-lg tw:mx-auto tw:font-normal">{{ format_price($price) }}</span>
+        <a href="#" class="title link tw:text-lg tw:mx-auto tw:font-semibold">{{ $title }}</a>
+        <span class="price tw:text-base tw:mx-auto tw:font-normal">{{ format_price($price) }}</span>
         <a class="tw:block tw:w-full tw:md:w-auto tw:text-sm tw:text-center tw:rounded-md tw:shadow-sm tw:border tw:border-transparent tw:duration-300 tw:hover:border-gray-700 tw:bg-sunset tw:px-2 tw:lg:px-3 tw:py-1 tw:mb-3 tw:mt-1 tw:mx-auto" href="#">@lang('QUICK ADD')</a>
         {{-- <ul class="list-color-product">
             <li class="list-color-item color-swatch active">
