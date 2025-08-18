154e9ec5dc0616bf9157813efc2a4043901360a6
yoonustehrani <yoonustehrani28@gmail.com>
2025-08-18 15:09:44 +0330

    ssl issues laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            Livewire::setUpdateRoute(function ($handle) {
+                $path = config('app.path').'/livewire/update';
+                return Route::post($path, $handle)->middleware('web');
+            });
+            Livewire::setScriptRoute(function ($handle) {
+                $path = config('app.path').'/livewire/livewire.js';
+                return Route::get($path, $handle)->middleware('web');
+            });
+            URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        }
     }
 
     /**
