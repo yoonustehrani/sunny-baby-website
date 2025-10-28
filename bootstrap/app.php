7dde06207b4110d306fa3f33e13e5a82a5b75c7c
yoonustehrani <yoonustehrani28@gmail.com>
2025-10-28 15:11:13 +0330

    trust proxies in laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustHosts(['*']);
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
