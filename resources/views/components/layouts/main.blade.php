f196c7001ba220551a49aeb40c3172985224b592
yoonustehrani <yoonustehrani28@gmail.com>
2025-11-19 15:38:26 +0330

    mobile menu similar to main menu

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -110,50 +110,21 @@
             <div class="mb-body">
                 <ul class="nav-ul-mb" id="wrapper-menu-navigation">
                     <li class="nav-mb-item">
-                        <a href="index.html#dropdown-menu-one" class="collapsed mb-menu-link current" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dropdown-menu-one">
-                            <span>Home</span>
-                            <span class="btn-open-sub"></span>
+                        <a href="{{ route('home') }}" class="mb-menu-link current">
+                            <span>@lang('Home')</span>
                         </a>
-                        <div id="dropdown-menu-one" class="collapse">
-                            <ul class="sub-nav-menu" >
-                                <li><a href="index.html" class="sub-nav-link">Home Fashion 01</a></li>
-                                <li><a href="home-02.html" class="sub-nav-link">Home Fashion 02</a></li>
-                                <li><a href="home-03.html" class="sub-nav-link">Home Fashion 03</a></li>
-                                <li><a href="home-04.html" class="sub-nav-link">Home Fashion 04</a></li>
-                                <li><a href="home-05.html" class="sub-nav-link">Home Fashion 05</a></li>
-                                <li><a href="home-06.html" class="sub-nav-link">Home Fashion 06</a></li>
-                                <li><a href="home-07.html" class="sub-nav-link">Home Fashion 07</a></li>
-                                <li><a href="home-08.html" class="sub-nav-link">Home Fashion 08</a></li>
-                                <li><a href="home-giftcard.html" class="sub-nav-link">Home Gift Card</a></li>
-                                <li><a href="home-headphone.html" class="sub-nav-link">Home Headphone</a></li>
-                                <li><a href="home-multi-brand.html" class="sub-nav-link">Home Multi Brand</a></li>
-                                <li><a href="home-skincare.html" class="sub-nav-link">Home skincare</a></li>
-                                <li><a href="home-headphone.html" class="sub-nav-link">Home Headphone</a></li>
-                                <li><a href="home-giftcard.html" class="sub-nav-link">Home Gift Card</a></li>
-                                <li><a href="home-furniture.html" class="sub-nav-link">Home Furniture</a></li>
-                                <li><a href="home-furniture-02.html" class="sub-nav-link">Home Furniture 2</a></li>
-                                <li><a href="home-skateboard.html" class="sub-nav-link">Home Skateboard</a></li>
-                                <li><a href="home-stroller.html" class="sub-nav-link">Home Stroller</a></li>
-                                <li><a href="home-decor.html" class="sub-nav-link">Home Decor</a></li>
-                                <li><a href="home-electronic.html" class="sub-nav-link">Home Electronic</a></li>
-                                <li><a href="home-setup-gear.html" class="sub-nav-link">Home Setup Gear</a></li>
-                                <li><a href="home-dog-accessories.html" class="sub-nav-link">Home Dog Accessories</a></li>
-                                <li><a href="home-kitchen-wear.html" class="sub-nav-link">Home Kitchen Wear</a></li>
-                                <li><a href="home-phonecase.html" class="sub-nav-link">Home Phonecase</a></li>
-                                <li><a href="home-paddle-boards.html" class="sub-nav-link">Home Paddle Boards</a></li>
-                                <li><a href="home-glasses.html" class="sub-nav-link">Home Glasses</a></li>
-                                <li><a href="home-pod-store.html" class="sub-nav-link">Home POD Store</a></li>
-                                <li><a href="home-activewear.html" class="sub-nav-link">Home Activewear</a></li>
-                                <li><a href="home-handbag.html" class="sub-nav-link">Home Handbag</a></li>
-                                <li><a href="home-tee.html" class="sub-nav-link">Home Tee</a></li>
-                                <li><a href="home-sock.html" class="sub-nav-link">Home Sock</a></li>
-                                <li><a href="home-jewerly.html" class="sub-nav-link">Home Jewelry</a></li>
-                                <li><a href="home-sneaker.html" class="sub-nav-link">Home Sneaker</a></li>
-                            </ul>
-                        </div>
-                        
                     </li>
                     <li class="nav-mb-item">
+                        <a href="{{ route('pages.shop') }}" class="mb-menu-link">
+                            <span>@lang('Shop')</span>
+                        </a>
+                    </li>
+                    <li class="nav-mb-item">
+                        <a href="{{ route('pages.contact') }}" class="mb-menu-link">
+                            <span>@lang('Contact Us')</span>
+                        </a>
+                    </li>
+                    {{-- <li class="nav-mb-item">
                         <a href="index.html#dropdown-menu-two" class="collapsed mb-menu-link current" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dropdown-menu-two">
                             <span>Shop</span>
                             <span class="btn-open-sub"></span>
@@ -210,157 +181,26 @@
                                 </li>
                             </ul>
                         </div>
-                    </li>
-                    <li class="nav-mb-item">
-                        <a href="index.html#dropdown-menu-three" class="collapsed mb-menu-link current" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dropdown-menu-three">
-                            <span>Products</span>
-                            <span class="btn-open-sub"></span>
-                        </a>
-                        <div id="dropdown-menu-three" class="collapse">
-                            <ul class="sub-nav-menu" id="sub-menu-navigation">
-                                <li>
-                                    <a href="index.html#sub-product-one" class="sub-nav-link collapsed"  data-bs-toggle="collapse" aria-expanded="true" aria-controls="sub-product-one">
-                                        <span>Product layouts</span>
-                                        <span class="btn-open-sub"></span>
-                                    </a>
-                                    <div id="sub-product-one" class="collapse">
-                                        <ul class="sub-nav-menu sub-menu-level-2">
-                                            <li><a href="product-detail.html" class="sub-nav-link">Product default</a></li>
-                                            <li><a href="product-grid-1.html" class="sub-nav-link">Product grid 1</a></li>
-                                            <li><a href="product-grid-2.html" class="sub-nav-link">Product grid 2</a></li>
-                                            <li><a href="product-stacked.html" class="sub-nav-link">Product stacked</a></li>
-                                            <li><a href="product-right-thumbnails.html" class="sub-nav-link">Product right thumbnails</a></li>
-                                            <li><a href="product-bottom-thumbnails.html" class="sub-nav-link">Product bottom thumbnails</a></li>
-                                            <li><a href="product-drawer-sidebar.html" class="sub-nav-link">Product drawer sidebar</a></li>
-                                            <li><a href="product-description-accordion.html" class="sub-nav-link">Product description accordion</a></li>
-                                            <li><a href="product-description-list.html" class="sub-nav-link">Product description list</a></li>
-                                            <li><a href="product-description-vertical.html" class="sub-nav-link">Product description vertical</a></li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a href="index.html#sub-product-two" class="sub-nav-link collapsed"  data-bs-toggle="collapse" aria-expanded="true" aria-controls="sub-product-two">
-                                        <span>Product details</span>
-                                        <span class="btn-open-sub"></span>
-                                    </a>
-                                    <div id="sub-product-two" class="collapse">
-                                        <ul class="sub-nav-menu sub-menu-level-2">
-                                            <li><a href="product-inner-zoom.html" class="sub-nav-link">Product inner zoom</a></li>
-                                            <li><a href="product-zoom-magnifier.html" class="sub-nav-link">Product zoom magnifier</a></li>
-                                            <li><a href="product-no-zoom.html" class="sub-nav-link">Product no zoom</a></li>
-                                            <li><a href="product-photoswipe-popup.html" class="sub-nav-link">Product photoswipe popup</a></li>
-                                            <li><a href="product-zoom-popup.html" class="sub-nav-link">Product external zoom and photoswipe popup</a></li>
-                                            <li><a href="product-video.html" class="sub-nav-link">Product video</a></li>
-                                            <li><a href="product-3d.html" class="sub-nav-link">Product 3D, AR models</a></li>
-                                            <li><a href="product-options-customizer.html" class="sub-nav-link">Product options & customizer</a></li>
-                                            <li><a href="product-advanced-types.html" class="sub-nav-link">Advanced product types</a></li>
-                                            <li><a href="product-giftcard.html" class="sub-nav-link">Recipient information form for gift card products</a></li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a href="index.html#sub-product-three" class="sub-nav-link collapsed"  data-bs-toggle="collapse" aria-expanded="true" aria-controls="sub-product-three">
-                                        <span>Product swatchs</span>
-                                        <span class="btn-open-sub"></span>
-                                    </a>
-                                    <div id="sub-product-three" class="collapse">
-                                        <ul class="sub-nav-menu sub-menu-level-2">
-                                            <li><a href="product-color-swatch.html" class="sub-nav-link">Product color swatch</a></li>
-                                            <li><a href="product-rectangle.html" class="sub-nav-link">Product rectangle</a></li>
-                                            <li><a href="product-rectangle-color.html" class="sub-nav-link">Product rectangle color</a></li>
-                                            <li><a href="product-swatch-image.html" class="sub-nav-link">Product swatch image</a></li>
-                                            <li><a href="product-swatch-image-rounded.html" class="sub-nav-link">Product swatch image rounded</a></li>
-                                            <li><a href="product-swatch-dropdown.html" class="sub-nav-link">Product swatch dropdown</a></li>
-                                            <li><a href="product-swatch-dropdown-color.html" class="sub-nav-link">Product swatch dropdown color</a></li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a href="index.html#sub-product-four" class="sub-nav-link collapsed"  data-bs-toggle="collapse" aria-expanded="true" aria-controls="sub-product-four">
-                                        <span>Product features</span>
-                                        <span class="btn-open-sub"></span>
-                                    </a>
-                                    <div id="sub-product-four" class="collapse">
-                                        <ul class="sub-nav-menu sub-menu-level-2">
-                                            <li><a href="product-frequently-bought-together.html" class="sub-nav-link">Frequently bought together</a></li>
-                                            <li><a href="product-frequently-bought-together-2.html" class="sub-nav-link">Frequently bought together 2</a></li>
-                                            <li><a href="product-upsell-features.html" class="sub-nav-link">Product upsell features</a></li>
-                                            <li><a href="product-pre-orders.html" class="sub-nav-link">Product pre-orders</a></li>
-                                            <li><a href="product-notification.html" class="sub-nav-link">Back in stock notification</a></li>
-                                            <li><a href="product-pickup.html" class="sub-nav-link">Product pickup</a></li>
-                                            <li><a href="product-images-grouped.html" class="sub-nav-link">Variant images grouped</a></li>
-                                            <li><a href="product-complimentary.html" class="sub-nav-link">Complimentary products</a></li>
-                                            <li><a href="product-quick-order-list.html" class="sub-nav-link line-clamp">Quick order list<div class="demo-label"><span class="demo-new">New</span></div></a></li>
-                                        </ul>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-mb-item">
-                        <a href="index.html#dropdown-menu-four" class="collapsed mb-menu-link current" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dropdown-menu-four">
-                            <span>Pages</span>
-                            <span class="btn-open-sub"></span>
-                        </a>
-                        <div id="dropdown-menu-four" class="collapse">
-                            <ul class="sub-nav-menu" id="sub-menu-navigation">
-                                <li><a href="about-us.html" class="sub-nav-link">About us</a></li>
-                                <li><a href="brands.html" class="sub-nav-link line-clamp">Brands<div class="demo-label"><span class="demo-new">New</span></div></a></li>
-                                <li><a href="brands-v2.html" class="sub-nav-link">Brands V2</a></li>
-                                <li><a href="contact-1.html" class="sub-nav-link">Contact 1</a></li>
-                                <li><a href="contact-2.html" class="sub-nav-link">Contact 2</a></li>
-                                <li><a href="faq-1.html" class="sub-nav-link">FAQ 01</a></li>
-                                <li><a href="faq-2.html" class="sub-nav-link">FAQ 02</a></li>
-                                <li><a href="our-store.html" class="sub-nav-link">Our store</a></li>
-                                <li><a href="store-locations.html" class="sub-nav-link">Store locator</a></li>
-                                <li><a href="timeline.html" class="sub-nav-link line-clamp">Timeline<div class="demo-label"><span class="demo-new">New</span></div></a></li>
-                                <li><a href="{{ route('cart') }}" class="sub-nav-link line-clamp">@lang("View cart")</a></li>
-                                <li><a href="checkout.html" class="sub-nav-link line-clamp">Check out</a></li>
-                                <li><a href="payment-confirmation.html" class="sub-nav-link line-clamp">Payment Confirmation</a></li>
-                                <li><a href="payment-failure.html" class="sub-nav-link line-clamp">Payment Failure</a></li>
-                                <li><a href="my-account.html" class="sub-nav-link line-clamp">My Account</a></li>
-                            </ul>
-                        </div>
-                        
-                    </li>
-                    <li class="nav-mb-item">
-                        <a href="index.html#dropdown-menu-five" class="collapsed mb-menu-link current" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dropdown-menu-five">
-                            <span>Blog</span>
-                            <span class="btn-open-sub"></span>
-                        </a>
-                        <div id="dropdown-menu-five" class="collapse">
-                            <ul class="sub-nav-menu" >
-                                <li><a href="blog-grid.html" class="sub-nav-link">Grid layout</a></li>
-                                <li><a href="blog-sidebar-left.html" class="sub-nav-link">Left sidebar</a></li>
-                                <li><a href="blog-sidebar-right.html" class="sub-nav-link">Right sidebar</a></li>
-                                <li><a href="blog-list.html" class="sub-nav-link">Blog list</a></li>
-                                <li><a href="blog-detail.html" class="sub-nav-link">Single Post</a></li>
-                            </ul>
-                        </div>
-                        
-                    </li>
-                    <li class="nav-mb-item">
-                        <a href="https://themeforest.net/item/ecomus-ultimate-html5-template/53417990?s_rank=3" class="mb-menu-link">Buy now</a>
-                    </li>
+                    </li> --}}
                 </ul>
                 <div class="mb-other-content">
-                    <div class="d-flex group-icon">
+                    {{-- <div class="d-flex group-icon">
                         <a href="wishlist.html" class="site-nav-icon"><i class="icon icon-heart"></i>Wishlist</a>
                         <a href="home-search.html" class="site-nav-icon"><i class="icon icon-search"></i>Search</a>
-                    </div>
-                    <div class="mb-notice">
+                    </div> --}}
+                    {{-- <div class="mb-notice">
                         <a href="contact-1.html" class="text-need">Need help ?</a>
                     </div>
                     <ul class="mb-info">
                         <li>Address: 1234 Fashion Street, Suite 567, <br> New York, NY 10001</li>
                         <li>Email: <b>info@fashionshop.com</b></li>
                         <li>Phone: <b>(212) 555-1234</b></li>
-                    </ul>
+                    </ul> --}}
                 </div>
             </div>
             <div class="mb-bottom">
-                <a href="login.html" class="site-nav-icon"><i class="icon icon-account"></i>Login</a>
-                <div class="bottom-bar-language">
+                <a href="{{ route('login') }}" class="site-nav-icon"><i class="icon icon-account"></i>@lang('Login')</a>
+                {{-- <div class="bottom-bar-language">
                     <div class="tf-currencies">
                         <select class="image-select center style-default type-currencies">
                             <option data-thumbnail="images/country/fr.svg">EUR <span>€ | France</span></option>
@@ -377,7 +217,7 @@
                             <option>اردو</option>
                         </select>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>       
     </div>
